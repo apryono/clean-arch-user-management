e592fb6613e0ea2431dfede9fe12faee5213b303
Apryono Boangmanalu <apryonoboang@gmail.com>
2020-02-23 12:05:40 +0700

    [FEATURE] adding feature response wraper

--- utils/response_wrapper.go
+++ utils/response_wrapper.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -14,4 +15,14 @@ func Message(status bool, message string) map[string]interface{} {
 func Response(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	_ = json.NewEncoder(w).Encode(data)
+}
+
+func TokenResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+// LogError to log error handling
+func LogError(function string, str string, err error) {
+	log.Printf("[%v] %v : %v", function, str, err)
 }
